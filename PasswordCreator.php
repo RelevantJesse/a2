24747a86a1af4f481607e9bb7ce3335d32318c58
Jesse Prescott <jesseprescott@yahoo.com>
2017-02-25 14:58:49 -0500

    Added password generation logic / cleanup

--- PasswordCreator.php
+++ PasswordCreator.php
@@ -1,36 +1,25 @@
 <?php
-
 namespace DWA{
   class PasswordCreator{
-
     public static function Generate(){
-      if(!isset($_GET['PasswordType']))
-      return;
-
-      $type = $_GET['PasswordType'];
-
-      if($type == "XKCD"){
-        return self::GenerateXKCD();
-      }
-      else{
-        return self::GenerateSchneier();
-      }
-    }
-
-    public static function GenerateXKCD(){
       $words = file("wordlist.txt") or die("Unable to open file!");
       $password = "";
 
-      for ($x = 0; $x < $_GET['ddlNumberOfWords']; $x++) {
+      for ($x = 0; $x < $_GET["ddlNumberOfWords"]; $x++) {
         $password .= trim($words[rand(0, count($words))]) . "-";
       }
 
       $password = rtrim($password, "-");
 
-      return $password;
-    }
+      if(isset($_GET["chkNumber"])){
+        $password .= rand(0,9);
+      }
 
-    public static function GenerateSchneier(){
+      if(isset($_GET["chkSymbol"])){
+        $password .= $_GET["ddlSymbols"];
+      }
+
+      return $password;
     }
   }
 }
--- index.php
+++ index.php
@@ -1,28 +1,21 @@
-<?php require('PasswordCreator.php')?>
+<?php
+require('PasswordCreator.php');
+require('Form.php');
+
+$form = new DWA\Form($_GET);
+?>
 
 <!DOCTYPE html>
 <html>
 <head>
 
-	<title>Password Generator</title>
+	<title>XKCD Password Generator</title>
 
   <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>
 	<link href='https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css' rel='stylesheet'>
 	<link href='css/main.css' rel='stylesheet'>
   <script src="scripts/jquery-3.1.1.min.js"></script>
   <script>
-    function ToggleType() {
-      var x = document.getElementById('XKCDDiv');
-      var s = document.getElementById('SchneierDiv');
-      if (x.style.display === 'none') {
-          x.style.display = 'block';
-          s.style.display = 'none';
-      } else {
-          x.style.display = 'none';
-          s.style.display = 'block';
-      }
-    }
-
     function ToggleSymbol() {
       var c = document.getElementById('chkSymbol');
       if (c.checked) {
@@ -35,61 +28,43 @@
   </script>
 </head>
 <body>
-<?php echo var_dump(DWA\PasswordCreator::Generate()) ?>
 <div class="maindiv">
 	<div class="centerdiv">
     <br>
-		<h1>Password Generator</h1>
+		<h1>XKCD Password Generator</h1>
     <form method='GET'>
-      <div class="radio-inline">
-        <label>
-          <input type="radio" name="PasswordType" id="optionsRadios1" value="XKCD" onclick="ToggleType();" checked>
-          XKCD
-        </label>
-      </div>
-      <div class="radio-inline">
-        <label>
-          <input type="radio" name="PasswordType" id="optionsRadios2" onclick="ToggleType();" value="Schneier">
-          Schneier Scheme
-        </label>
-      </div>
-    </div>
-  <br><br>
+
 
   <div id="XKCDDiv">
-    <div class="centerdiv">
-      <img src='images/password_strength.png' alt='Password Strength'>
-    </div>
-    <br>
     <br>
     <div class="innerdiv">
         <div class="form-group">
           <label for="ddlNumberOfWords">Number of words:</label>
           <select class="form-control" name="ddlNumberOfWords" id="ddlNumberOfWords">
-              <option>3</option>
-              <option selected="True">4</option>
-              <option>5</option>
-              <option>6</option>
-              <option>7</option>
-              <option>8</option>
+              <option <?php if($form->isSubmitted() && $_GET["ddlNumberOfWords"] == "3") echo "selected"; ?>>3</option>
+              <option <?php if(!$form->isSubmitted() || $_GET["ddlNumberOfWords"] == "4") echo "selected"; ?>>4</option>
+              <option <?php if($form->isSubmitted() && $_GET["ddlNumberOfWords"] == "5") echo "selected"; ?>>5</option>
+              <option <?php if($form->isSubmitted() && $_GET["ddlNumberOfWords"] == "6") echo "selected"; ?>>6</option>
+              <option <?php if($form->isSubmitted() && $_GET["ddlNumberOfWords"] == "7") echo "selected"; ?>>7</option>
+              <option <?php if($form->isSubmitted() && $_GET["ddlNumberOfWords"] == "8") echo "selected"; ?>>8</option>
             </select>
         </div>
         <div class="form-group">
           <label>Options:</label>
           <div class="checkbox">
-            <label><input type="checkbox" name="chkNumber">Include a number</label>
+            <label><input type="checkbox" name="chkNumber" <?php if(isset($_GET["chkNumber"])) echo "checked"; ?>>Include a number</label>
           </div>
           <div class="checkbox">
-            <label><input type="checkbox" name="chkSymbol" id="chkSymbol" onclick="ToggleSymbol();">Include a symbol</label>
+            <label><input type="checkbox" name="chkSymbol" id="chkSymbol" onclick="ToggleSymbol();" <?php if(isset($_GET["chkSymbol"])) echo "checked"; ?>>Include a symbol</label>
           </div>
-          <select class="form-control" name="ddlSymbols" id="ddlSymbols" width=50px disabled="true">
-            <option>!</option>
-            <option>@</option>
-            <option>#</option>
-            <option>$</option>
-            <option>%</option>
-            <option>^</option>
-            <option>&amp;</option>
+          <select class="form-control" name="ddlSymbols" id="ddlSymbols" width=50px <?php if(!isset($_GET["chkSymbol"])) echo "disabled=\"true\""; ?>">
+            <option <?php if($form->isSubmitted() && $_GET["ddlSymbols"] == "!") echo "selected"; ?>>!</option>
+            <option <?php if($form->isSubmitted() && $_GET["ddlSymbols"] == "@") echo "selected"; ?>>@</option>
+            <option <?php if($form->isSubmitted() && $_GET["ddlSymbols"] == "#") echo "selected"; ?>>#</option>
+            <option <?php if($form->isSubmitted() && $_GET["ddlSymbols"] == "$") echo "selected"; ?>>$</option>
+            <option <?php if($form->isSubmitted() && $_GET["ddlSymbols"] == "%") echo "selected"; ?>>%</option>
+            <option <?php if($form->isSubmitted() && $_GET["ddlSymbols"] == "^") echo "selected"; ?>>^</option>
+            <option <?php if($form->isSubmitted() && $_GET["ddlSymbols"] == "&") echo "selected"; ?>>&amp;</option>
             <option>*</option>
           </select>
         </div>
@@ -97,14 +72,15 @@
     </div>
   </div>
 
-  <div id="SchneierDiv">
-
-  </div>
-
 <div class="innerdiv">
   <button type="submit" class="btn btn-primary">Generate Password</button>
 </div>
 <br>
 </form>
+<?php if($form->isSubmitted()){ ?>
+<div class="alert alert-success centerdiv" role="alert">
+  <?php echo "Password Generated: " . DWA\PasswordCreator::Generate() ?>
+</div>
+<?php } ?>
 </body>
 </html>
